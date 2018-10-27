d3cc320b98e75acad452ae883ff8cdc0ecdcedbf
plosana <patricia.losana@estudiante.uam.es>
2018-10-27 18:34:19 +0200

    updated entrenamiento and clasificaP

--- code/Clasificador.py
+++ code/Clasificador.py
@@ -1,47 +1,48 @@
 from abc import ABCMeta,abstractmethod
 import numpy as np
-from tabulate import tabulate
+#from tabulate import tabulate
 
 class Clasificador(object):
 
-  # Clase abstracta
-  __metaclass__ = ABCMeta
+    # Clase abstracta
+    __metaclass__ = ABCMeta
 
-  # Metodos abstractos que se implementan en casa clasificador concreto
-  @abstractmethod
-  # TODO: esta funcion deben ser implementadas en cada clasificador concreto
-  # datosTrain: matriz numpy con los datos de entrenamiento
-  # atributosDiscretos: array bool con la indicatriz de los atributos nominales
-  # diccionario: array de diccionarios de la estructura Datos utilizados para la codificacion
-  # de variables discretas
-  def entrenamiento(self,datosTrain,atributosDiscretos,diccionario):
-    pass
-
-
-  @abstractmethod
-  # TODO: esta funcion deben ser implementadas en cada clasificador concreto
-  # devuelve un numpy array con las predicciones
-  def clasifica(self,datosTest,atributosDiscretos,diccionario):
-    pass
+    # Metodos abstractos que se implementan en casa clasificador concreto
+    @abstractmethod
+    # TODO: esta funcion deben ser implementadas en cada clasificador concreto
+    # datosTrain: matriz numpy con los datos de entrenamiento
+    # atributosDiscretos: array bool con la indicatriz de los atributos nominales
+    # diccionario: array de diccionarios de la estructura Datos utilizados para la codificacion de variables discretas
+    def entrenamiento(self,datosTrain,atributosDiscretos,diccionario):
+        pass
 
 
-  # Obtiene el numero de aciertos y errores para calcular la tasa de fallo
-  # TODO: implementar
-#   def error(self,datos,pred):
-#     # Aqui se compara la prediccion (pred) con las clases reales y se calcula el error
-#     pass
+    @abstractmethod
+    # TODO: esta funcion deben ser implementadas en cada clasificador concreto
+    # devuelve un numpy array con las predicciones
+    def clasifica(self,datosTest,atributosDiscretos,diccionario):
+        pass
 
 
-  # Realiza una clasificacion utilizando una estrategia de particionado determinada
-  # TODO: implementar esta funcion
-#   def validacion(self,particionado,dataset,clasificador,seed=None):
 
-#     # Creamos las particiones siguiendo la estrategia llamando a particionado.creaParticiones
-#     # - Para validacion cruzada: en el bucle hasta nv entrenamos el clasificador con la particion de train i
-#     # y obtenemos el error en la particion de test i
-#     # - Para validacion simple (hold-out): entrenamos el clasificador con la particion de train
-#     # y obtenemos el error en la particion test
-# 	pass
+    # Obtiene el numero de aciertos y errores para calcular la tasa de fallo
+    # TODO: implementar
+    def error(self,datos,pred):
+    #     # Aqui se compara la prediccion (pred) con las clases reales y se calcula el error
+        pass
+
+
+
+    # Realiza una clasificacion utilizando una estrategia de particionado determinada
+    # TODO: implementar esta funcion
+    def validacion(self,particionado,dataset,clasificador,seed=None):
+
+    #     # Creamos las particiones siguiendo la estrategia llamando a particionado.creaParticiones
+    #     # - Para validacion cruzada: en el bucle hasta nv entrenamos el clasificador con la particion de train i
+    #     # y obtenemos el error en la particion de test i
+    #     # - Para validacion simple (hold-out): entrenamos el clasificador con la particion de train
+    #     # y obtenemos el error en la particion test
+        pass
 
 
 ##############################################################################
@@ -57,9 +58,12 @@ class ClasificadorNaiveBayes(Clasificador):
         columns = np.column_stack(datostrain)
         probabilidades = []
         clas = columns[-1]
-        ini_clas = {}        
+        ini_clas = {}
+        c, counts = np.unique(columns[-1], return_counts=True)
+        p_class = dict(zip(c.astype(int), counts))
+       
         for key in diccionario["Class"]:
-            ini_clas.update({int(diccionario["Class"][key]):1})
+            ini_clas.update({int(diccionario["Class"][key]):0})
         for key in diccionario:
             if key == "Class":
                 pass
@@ -77,8 +81,9 @@ class ClasificadorNaiveBayes(Clasificador):
            
                 
                 for value in probabilidades[index_col]:
+                   
                     for i in probabilidades[index_col][value]:
-                        probabilidades[index_col][value][i] = probabilidades[index_col][value][i] / len(columns[index_col])
+                        probabilidades[index_col][value][i] = round(probabilidades[index_col][value][i] / p_class[i], 2)
 
 
             else:
@@ -120,6 +125,31 @@ class ClasificadorNaiveBayes(Clasificador):
                     max = prob[diccionario["Class"][clas]]
                     decision = clas
             clasificador.append({decision:max})
+            
+            
+    def clasificaP(self, datostest,atributosDiscretos,diccionario):
+        columns = np.column_stack(datostest)
+        c, counts = np.unique(columns[-1], return_counts=True)
+        p_class = dict(zip(c.astype(int), counts))
+
+        clasifica = {}
+        clas = columns[-1]
+        for index_col in range(len(columns[:-1])):
+            if atributosDiscretos[index_col] == "Nominal":
+                for value in self.probabilidades[index_col]:   
+                    posterior, num = [], []
+                    for i in self.probabilidades[index_col][value]:
+                       
+                        verosimilitud = self.probabilidades[index_col][value][i] 
+                        prior = p_class[i] / len(columns[index_col])
+                        num.append(verosimilitud * prior)
+                    den = sum(num)
+                    if (den > 0):
+                        for x in num:
+                            posterior.append(round (x / den, 2))
+                        print(posterior.index(max(posterior)), max(posterior),  posterior)
+                        clasifica.update({posterior.index(max(posterior)):max(posterior)})
         
+        return clasifica
                 
-        print(clasificador)
+        #print(clasificador)
--- code/EstrategiaParticionado.py
+++ code/EstrategiaParticionado.py
@@ -1,9 +1,6 @@
 from abc import ABCMeta,abstractmethod
-<<<<<<< HEAD:Plantillas/EstrategiaParticionado.py
-
-=======
 import numpy as np
->>>>>>> ccd947ceaf0edbe860ff894d1c83aacb534d6559:code/EstrategiaParticionado.py
+import random
 
 class Particion():
 
@@ -18,10 +15,6 @@ class EstrategiaParticionado(object):
 
   # Clase abstracta
   __metaclass__ = ABCMeta
-<<<<<<< HEAD:Plantillas/EstrategiaParticionado.py
-  numeroParticiones
-=======
->>>>>>> ccd947ceaf0edbe860ff894d1c83aacb534d6559:code/EstrategiaParticionado.py
 
 
   # Atributos: deben rellenarse adecuadamente para cada estrategia concreta: nombreEstrategia, numeroParticiones, listaParticiones. Se pasan en el constructor
@@ -36,27 +29,6 @@ class EstrategiaParticionado(object):
 
 class ValidacionSimple(EstrategiaParticionado):
 
-<<<<<<< HEAD:Plantillas/EstrategiaParticionado.py
-  # Crea particiones segun el metodo tradicional de division de los datos segun el porcentaje deseado.
-  # Devuelve una lista de particiones (clase Particion)
-  # TODO: implementar
-
-  def __init__(self, nombreEstrategia, numeroParticiones, listaParticiones, porcentaje):
-      self.nombreEstrategia = nombreEstrategia
-      self.numeroParticiones = numeroParticiones
-      self.listaParticiones = listaParticiones
-      self.porcentaje = porcentaje
-
-
-  def creaParticiones(self,datos,seed=None):
-    par = Particion()
-    perms = np.indices(self.numeroParticiones,len(datos))
-    for ar in perms:
-        permutacion = np.random.permutation(ar)
-        par.indicesTrain.append(permutacion[:self.porcentaje*len(datos)])
-        par.indicesTest.append(permutacion[self.porcentaje*len(datos):])
-    return par
-=======
     # Crea particiones segun el metodo tradicional de division de los datos segun el porcentaje deseado.
     # Devuelve una lista de particiones (clase Particion)
     # TODO: implementar
@@ -80,7 +52,6 @@ class ValidacionSimple(EstrategiaParticionado):
             self.particiones[particion] = self.creaParticiones(dataset)
 
     
->>>>>>> ccd947ceaf0edbe860ff894d1c83aacb534d6559:code/EstrategiaParticionado.py
 
 
 #####################################################################################################
@@ -90,25 +61,6 @@ class ValidacionCruzada(EstrategiaParticionado):
   # El conjunto de entrenamiento se crea con las nfolds-1 particiones y el de test con la particion restante
   # Esta funcion devuelve una lista de particiones (clase Particion)
   # TODO: implementar
-<<<<<<< HEAD:Plantillas/EstrategiaParticionado.py
-
-  def __init__(self, nombreEstrategia, numeroParticiones, listaParticiones):
-      self.nombreEstrategia = nombreEstrategia
-      self.numeroParticiones = numeroParticiones
-      self.listaParticiones = listaParticiones
-
-  def creaParticiones(self,datos,seed=None):
-    par = Particion()
-    perms = np.arange(len(datos))
-    for i in range(0,len(datos)/self.numeroParticiones, self.numeroParticiones):
-        par.indicesTest.append(perms[i:i+self.numeroParticiones])
-        par.indicesTrain.append(perms[:i] + perms[i+self.numeroParticiones:])
-
-    return par
-
-
-#####################################################################################################
-=======
     def creaParticiones(self,datos,seed=None):
         particiones = {}
         perms = np.arange(len(datos.datos))
@@ -132,28 +84,30 @@ class ValidacionCruzada(EstrategiaParticionado):
   
 
     #####################################################################################################
->>>>>>> ccd947ceaf0edbe860ff894d1c83aacb534d6559:code/EstrategiaParticionado.py
 class ValidacionBootstrap(EstrategiaParticionado):
-
+    
   # Crea particiones segun el metodo de validacion por bootstrap.
   # Esta funcion devuelve una lista de particiones (clase Particion)
+  
   # TODO: implementar
-  def __init__(self, nombreEstrategia, numeroParticiones, listaParticiones, numTablas):
-      self.nombreEstrategia = nombreEstrategia
-      self.numeroParticiones = numeroParticiones
-      self.listaParticiones = listaParticiones
-      self.numTablas = numTablas
+      
+    def creaParticiones(self, datos,seed=None):
+        particiones = {}
+        perms = np.arange(len(datos.datos))
+        for i in range(self.numeroParticiones):
+            par = Particion()
+            for _ in range(self.tamParticion):
+                par.indicesTest = np.append(par.indicesTest,random.choice(perms)).astype(int)
+            par.indicesTrain = np.setdiff1d(perms, par.indicesTest)
+            particiones[i] = par
+        return particiones
+    
+    
+    def __init__(self, nombreEstrategia, numeroParticiones, tamParticion, dataset):
+        self.nombreEstrategia = nombreEstrategia
+        self.numeroParticiones = numeroParticiones
+        self.tamParticion = tamParticion
+        self.particiones = {}
+        self.particiones = self.creaParticiones(dataset)
 
-  def creaParticiones(self,datos,seed=None):
-    par = Particion()
-
-    for i in range(self.numTablas):
-        perms = np.arange(len(datos))
-        test = []
-        for j in range(self.numeroParticiones):
-            np.random.shuffle(perms)
-            test.append(perms[j])
-        par.indicesTest.append(test)
-        par.indicesTrain.append(list(set(perms) - set(test)))
-
-    return par
+  
