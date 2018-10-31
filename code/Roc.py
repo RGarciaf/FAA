5c41a6b2ba951e2d5d18ff92cd2b129fd69bce63
Roman Garcia <r.garcia.fernandez@accenture.com>
2018-10-31 11:47:09 +0100

    Actualizado Roc.py, ahora hace las 3 validaciones

--- code/Roc.py
+++ code/Roc.py
@@ -7,19 +7,90 @@ import re
 
 class Roc(object):
     def __init__(self):
-        roc = []
+        self.simple = []
+        self.cruzada = []
+        self.bootstrap = []
+    
+    def medias_roc(self):
+        balloons = Datos("ConjuntosDatos/balloons.data")
+        german = Datos("ConjuntosDatos/german.data")
+        tic_tac_toe = Datos("ConjuntosDatos/tic-tac-toe.data")
+        
+        print("Validacion simple")
+        simple = []
         estrategia = ValidacionSimple()
-        clas = ClasificadorNaiveBayes()
-        dataset = Datos("ConjuntosDatos/balloons.data")
-        roc.append(clas.roc(estrategia,dataset,clas))
+        clas = ClasificadorNaiveBayes()        
+        print("\tBalloons")
+        simple.append(clas.roc(estrategia,balloons,clas))
 
-        dataset = Datos("ConjuntosDatos/german.data")
+        print("\tGerman")
         estrategia = ValidacionSimple()
         clas = ClasificadorNaiveBayes()
-        roc.append(clas.roc(estrategia,dataset,clas))
+        simple.append(clas.roc(estrategia,german,clas))
 
-        dataset = Datos("ConjuntosDatos/tic-tac-toe.data")
+        print("\tTic-tac-toe")
         estrategia = ValidacionSimple()
         clas = ClasificadorNaiveBayes()
-        roc.append(clas.roc(estrategia,dataset,clas))
-        self.roc = roc
+        simple.append(clas.roc(estrategia,tic_tac_toe,clas))
+        self.simple = simple
+
+        print("Validacion cruzada")
+        cruzada = []
+        estrategia = ValidacionCruzada()
+        clas = ClasificadorNaiveBayes()
+        print("\tBalloons")
+        cruzada.append(clas.roc(estrategia,balloons,clas))
+
+        estrategia = ValidacionCruzada()
+        clas = ClasificadorNaiveBayes()
+        print("\tGerman")
+        cruzada.append(clas.roc(estrategia,german,clas))
+
+        estrategia = ValidacionCruzada()
+        clas = ClasificadorNaiveBayes()
+        print("\tTic-tac-toe")
+        cruzada.append(clas.roc(estrategia,tic_tac_toe,clas))
+        self.cruzada = cruzada
+
+        print("Validacion Bootstrap")
+        bootstrap = []
+        estrategia = ValidacionBootstrap()
+        clas = ClasificadorNaiveBayes()   
+        print("\tBalloons")     
+        bootstrap.append(clas.roc(estrategia,balloons,clas))
+
+        estrategia = ValidacionBootstrap()
+        clas = ClasificadorNaiveBayes()    
+        print("\tGerman")    
+        bootstrap.append(clas.roc(estrategia,german,clas))
+
+        estrategia = ValidacionBootstrap()
+        clas = ClasificadorNaiveBayes()  
+        print("\tTic-tac-toe")      
+        bootstrap.append(clas.roc(estrategia,tic_tac_toe,clas))
+        self.bootstrap = bootstrap
+
+        return simple, cruzada, bootstrap
+
+
+
+
+
+
+
+
+        # estrategia = ValidacionSimple()
+        # clas = ClasificadorNaiveBayes()
+        # dataset = Datos("ConjuntosDatos/balloons.data")
+        # roc.append(clas.roc(estrategia,dataset,clas))
+
+        # dataset = Datos("ConjuntosDatos/german.data")
+        # estrategia = ValidacionSimple()
+        # clas = ClasificadorNaiveBayes()
+        # roc.append(clas.roc(estrategia,dataset,clas))
+
+        # dataset = Datos("ConjuntosDatos/tic-tac-toe.data")
+        # estrategia = ValidacionSimple()
+        # clas = ClasificadorNaiveBayes()
+        # roc.append(clas.roc(estrategia,dataset,clas))
+        # self.roc = roc
